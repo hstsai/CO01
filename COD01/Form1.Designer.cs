3b4566c9c9defd43153b278776d2b96c703e3eac
蔡旭昇 <ths@stu.edu.tw>
2019-02-22 14:31:53 +0800

    加入專案檔案。

---
+++ COD01/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace COD01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.treeView1 = new System.Windows.Forms.TreeView();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+      this.SuspendLayout();
+      // 
+      // treeView1
+      // 
+      this.treeView1.LabelEdit = true;
+      this.treeView1.Location = new System.Drawing.Point(116, 13);
+      this.treeView1.Margin = new System.Windows.Forms.Padding(4);
+      this.treeView1.Name = "treeView1";
+      this.treeView1.Size = new System.Drawing.Size(382, 289);
+      this.treeView1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(13, 310);
+      this.button1.Margin = new System.Windows.Forms.Padding(4);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(485, 56);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "載入XML文件";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Enabled = false;
+      this.button2.Location = new System.Drawing.Point(13, 374);
+      this.button2.Margin = new System.Windows.Forms.Padding(4);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(485, 56);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "匯出XML文件";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.Enabled = false;
+      this.button3.Location = new System.Drawing.Point(13, 13);
+      this.button3.Margin = new System.Windows.Forms.Padding(4);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(95, 40);
+      this.button3.TabIndex = 3;
+      this.button3.Text = "↑";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Enabled = false;
+      this.button4.Location = new System.Drawing.Point(13, 61);
+      this.button4.Margin = new System.Windows.Forms.Padding(4);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(95, 40);
+      this.button4.TabIndex = 5;
+      this.button4.Text = "↓";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button5
+      // 
+      this.button5.Enabled = false;
+      this.button5.Location = new System.Drawing.Point(13, 212);
+      this.button5.Margin = new System.Windows.Forms.Padding(4);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(95, 40);
+      this.button5.TabIndex = 6;
+      this.button5.Text = "+";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
+      // 
+      // button6
+      // 
+      this.button6.Enabled = false;
+      this.button6.Location = new System.Drawing.Point(13, 260);
+      this.button6.Margin = new System.Windows.Forms.Padding(4);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(95, 40);
+      this.button6.TabIndex = 7;
+      this.button6.Text = "-";
+      this.button6.UseVisualStyleBackColor = true;
+      this.button6.Click += new System.EventHandler(this.button6_Click);
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.Filter = "XML檔案(*.xml)|*.xml";
+      this.openFileDialog1.Title = "開啟舊檔";
+      // 
+      // saveFileDialog1
+      // 
+      this.saveFileDialog1.Filter = "XML檔案(*.xml)|*.xml";
+      this.saveFileDialog1.Title = "另存新檔";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(508, 444);
+      this.Controls.Add(this.button6);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.treeView1);
+      this.Margin = new System.Windows.Forms.Padding(4);
+      this.Name = "Form1";
+      this.Text = "XML瀏覽器";
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+  }
+}
+
---
+++ COD01/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml; // XML文件
+using System.Xml.Linq; // LINQ TO XML
+
+namespace COD01
+{
+  public partial class Form1 : Form
+  {
+    XElement xe; // 讀取XML檔案
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    // 讀取XML檔案
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (openFileDialog1.ShowDialog() == DialogResult.OK) // 開檔對話窗
+      {
+        treeView1.BeginUpdate(); // 開啟更新狀態，暫停畫面重劃
+        treeView1.Nodes.Clear(); // 清除treeView1的所有節點
+        xe = XElement.Load(openFileDialog1.FileName); // 讀入XML文件檔
+        var trn = new TreeNode(xe.Name.ToString()); // 以XML文件根節點名稱建立TreeView節點(或TreeNode物件) https://msdn.microsoft.com/zh-tw/library/system.windows.forms.treeview(v=vs.110).aspx
+        treeView1.Nodes.Add(trn); // 新增TreeView控制項的根節點
+        AddNode(xe.Elements(), trn); // 呼叫遞迴函數來新增節點
+        treeView1.EndUpdate(); // 結束更新狀態，開始重劃畫面
+        treeView1.ExpandAll(); // 開啟所有節點
+
+        button2.Enabled = true; // 啟用匯出按鈕
+        button3.Enabled = true; // 啟用上移按鈕
+        button4.Enabled = true; // 啟用下移按鈕
+        button5.Enabled = true; // 啟用新增按鈕
+        button6.Enabled = true; // 啟用刪除按鈕
+      }
+    }
+
+    // 新增TreeView節點遞迴函數
+    private void AddNode(IEnumerable<XElement> xes, TreeNode treeNode)
+    {
+      foreach (XElement xn in xes) // 取出XML所有節點
+      {
+        var tn = new TreeNode(xn.Name.ToString()); // 建立TreeNode物件
+        treeNode.Nodes.Add(tn); // 新增TreeView控制項的節點
+
+        if (xn.Elements().Count() > 0) // 還有子節點，繼續
+        {
+          AddNode(xn.Elements(), tn);  // 呼叫遞迴函數
+        }
+        else
+        {
+          tn.Nodes.Add(xn.Value.ToString()); // 沒有子節點則回傳值
+        }
+      }
+    }
+
+    // 上移事件－針對TreeView節點
+    private void button3_Click(object sender, EventArgs e)
+    {
+      var tn = treeView1.SelectedNode; // 取得選取節點
+      if (tn != null && tn.Parent != null) // 沒有選定或選定為根節點
+      {
+        while (tn.Text != "book") // 向上找出book節點
+        {
+          tn = tn.Parent;
+        }
+
+        var pn = tn.PrevNode; // 取得前一節點
+        if (pn != null) // 第一個book節點無法上移
+        { 
+          treeView1.BeginUpdate(); // 開啟更新狀態，暫停畫面重劃
+          tn.Parent.Nodes.Remove(pn); // 刪除前節點
+          tn.Parent.Nodes.Insert(tn.Parent.Nodes.IndexOf(tn) + 1, pn); // 插入目前節點之後
+          treeView1.EndUpdate(); // 結束更新狀態，開始重劃畫面
+        }
+      }
+    }
+
+    // 下移事件－針對TreeView節點
+    private void button4_Click(object sender, EventArgs e)
+    {
+      var tn = treeView1.SelectedNode; // 取得選取節點
+      if (tn != null && tn.Parent != null) // 沒有選定或選定為根節點
+      {
+        while (tn.Text != "book") // 向上找出book節點
+        {
+          tn = tn.Parent;
+        }
+
+        var nn = tn.NextNode; // 取得下一節點
+        if (nn != null) // 最後一個book節點無法下移
+        {
+          treeView1.BeginUpdate(); // 開啟更新狀態，暫停畫面重劃
+          tn.Parent.Nodes.Remove(nn); // 刪除後節點
+          tn.Parent.Nodes.Insert(tn.Parent.Nodes.IndexOf(tn), nn); // 插入目前節點之前
+          treeView1.EndUpdate(); // 結束更新狀態，開始重劃畫面
+        }
+      }
+    }
+
+    // 新增事件
+    private void button5_Click(object sender, EventArgs e)
+    {
+      if (openFileDialog1.ShowDialog() == DialogResult.OK) // 開檔對話窗
+      {
+        var xenew = XElement.Load(openFileDialog1.FileName); // 讀入XML文件檔
+        foreach (XElement xn in xenew.Elements("book")) // 由新XML文件取出節點
+        {
+          var xo = from x in xe.Elements("book") // 篩選舊文件重複節點
+                   where x.Element("book_id").Value.Equals(xn.Element("book_id").Value)
+                   select x;             
+
+          if (xo.Count() > 0) // book_id重複
+          {
+            if (MessageBox.Show(xn.Element("book_id").Value + "重複，確認覆蓋嗎？", "XML瀏覽器", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes) // 提示重複對話窗
+            {
+              xo.First().ReplaceWith(xn); // 只有一個重複
+            }
+          }
+          else
+          {
+            xe.Add(xn); // 新增XElement節點
+          }
+        }
+
+        treeView1.BeginUpdate(); // 開啟更新狀態，暫停畫面重劃
+        treeView1.Nodes.Clear(); // 清除treeView1的所有節點，重建treeView1節點
+        var trn = new TreeNode(xe.Name.ToString()); // 以XML文件根節點名稱建立TreeView節點
+        treeView1.Nodes.Add(trn); // 新增TreeView控制項的根節點
+        AddNode(xe.Elements(), trn); // 呼叫遞迴函數來新增節點
+        treeView1.EndUpdate(); // 結束更新狀態，開始重劃畫面
+        treeView1.ExpandAll(); // 開啟所有節點
+      }
+    }
+
+    // 刪除事件
+    private void button6_Click(object sender, EventArgs e)
+    {
+      var tn = treeView1.SelectedNode; // 取得選取節點
+      if (tn != null && tn.Parent != null) // 沒有選定或選定為根節點
+      {
+        while (tn.Text != "book") // 向上找出TreeNode的book節點
+        {
+          tn = tn.Parent;
+        }
+        var tid = (from t in tn.Nodes.OfType<TreeNode>()
+                  where t.Text.Contains("book_id")
+                  select t.FirstNode.Text).First();
+        var xo = (from x in xe.Elements("book") // 找出舊XML文件book_id節點
+                 where x.Element("book_id").Value.Equals(tid)
+                 select x).First();
+        if (MessageBox.Show("確認要刪除" + xo.Element("book_title").Value + "(" + tid + ")嗎？", "XML瀏覽器", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes) // 提示刪除對話窗
+        {
+          xo.Remove(); // 只有一個重複
+        }
+
+        treeView1.BeginUpdate(); // 開啟更新狀態，暫停畫面重劃
+        treeView1.Nodes.Clear(); // 清除treeView1的所有節點，重建treeView1節點
+        var trn = new TreeNode(xe.Name.ToString()); // 以XML文件根節點名稱建立TreeView節點
+        treeView1.Nodes.Add(trn); // 新增TreeView控制項的根節點
+        AddNode(xe.Elements(), trn); // 呼叫遞迴函數來新增節點
+        treeView1.EndUpdate(); // 結束更新狀態，開始重劃畫面
+        treeView1.ExpandAll(); // 開啟所有節點
+      }
+    }
+
+    // 寫回XML檔案
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if (saveFileDialog1.ShowDialog() == DialogResult.OK) // 存檔對話窗
+      {
+        xe.Save(saveFileDialog1.FileName); // 匯出舊XML文件
+      }
+    }
+  }
+}
